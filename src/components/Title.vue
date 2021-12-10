4afd0ce8ece21bb1c12d812250888c7469bcfdff
AnaisDiGenua <anais.digenua5@gmail.com>
2021-12-10 19:24:40 +0100

    title

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>la mia prima app con Vue!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Title'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: teal;
+    }
+
+</style>
